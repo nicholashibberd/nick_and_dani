ad0408a9857822a114cc29e962e92bc66719e5b8
nicholashibberd <nicholashibberd@gmail.com>
2013-04-04 07:33:09 +0000

    serving the content from an actual html file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var url = require('url');
+var fs = require('fs')
 var queryString = require('querystring');
 
 function start(route) {
@@ -9,9 +10,11 @@ function start(route) {
 
 		route(pathname);
 
-		response.writeHead(200, {"Content-Type": "text/plain"});
-		response.write("Hello World!!! \n");
-		response.end();
+		fs.readFile('app/views/index.html', function(err, page) {
+			response.writeHead(200, {"Content-Type": "text/html"});
+			response.write(page);
+			response.end();
+		});
 	}
 	var port = process.env.PORT || 5000;
 
