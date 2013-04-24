cf0f4d4590ce4ed9ee3e9a14e7e5371232623799
nicholashibberd <nicholashibberd@gmail.com>
2013-04-24 07:58:58 +0000

    server responds to hello world

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,16 @@
 "use strict";
+var http = require("http");
+var server;
 
-exports.number = function() {
-	return 3;
+exports.start = function() {
+	server = http.createServer();
+
+	server.on("request", function(request, response) {
+		response.end("Hello World");
+	});
+	server.listen(8080);
+};
+
+exports.stop = function(callback) {
+	server.close(callback);
 };
