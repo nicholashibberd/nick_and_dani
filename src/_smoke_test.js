25bab8cd50c601781ada9d0843efce0999e4f3f4
nicholashibberd <nicholashibberd@gmail.com>
2013-05-20 20:39:39 +0000

    end of episode 30

--- src/_smoke_test.js
+++ src/_smoke_test.js
@@ -4,6 +4,7 @@
 	var child_process = require('child_process');
 	var http = require('http');
 	var child;
+	var fs = require('fs');
 
 	exports.setUp = function(done) {
 		runServer(done);
@@ -17,7 +18,7 @@
 	};
 
 	exports.test_canGetHomepage = function(test) {
-		httpGet("http://localhost:8080", function(response, receivedData) {
+		httpGet("http://localhost:5000", function(response, receivedData) {
 			var foundHomepage = receivedData.indexOf("Nick and Dani homepage") !== -1;
 			test.ok(foundHomepage, "Should have contained Nick and Dani hompepage");
 			test.done();
@@ -25,7 +26,7 @@
 	};
 
 	exports.test_canGet404Page = function(test) {
-		httpGet("http://localhost:8080/nonexistant.html", function(response, receivedData) {
+		httpGet("http://localhost:5000/nonexistant.html", function(response, receivedData) {
 			var foundHomepage = receivedData.indexOf("Page not found") !== -1;
 			test.ok(foundHomepage, "Should have contained 404 content");
 			test.done();
@@ -33,11 +34,30 @@
 	};
 
 	function runServer(callback) {
-		child = child_process.spawn("node", ["src/server/nickanddani", "8080"]);
+		var commandLine = parseProcfile();
+		child = child_process.spawn(commandLine[0], commandLine.slice(1));
 		child.stdout.setEncoding("utf8");
 		child.stdout.on("data", function(chunk) {
-			if (chunk.trim() === "Server started") callback();
+			if (chunk.trim().indexOf("Server started") !== -1) callback();
 		});
+	}
+
+	function parseProcfile() {
+		var procfile = fs.readFileSync('Procfile', "utf8");
+		var matches = procfile.trim().match(/^web:(.+)$/);
+		if (matches === null) throw "Could not parse Procfile";
+		var commandLine = matches[1];
+
+		var args = commandLine.split(' ');
+		args = args.filter(function(element) {
+			return (element.trim() !== "");
+		});
+		args = args.map(function(element) {
+			if (element === "$PORT") return "5000";
+			else return element;
+		});
+
+		return args;
 	}
 
 	function httpGet(url, callback) {
