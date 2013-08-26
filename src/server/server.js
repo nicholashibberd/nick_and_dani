23701f78920d4593f013e58c7a2bb0f4c2a3fa84
nicholashibberd <nicholashibberd@gmail.com>
2013-08-26 11:49:56 +0000

    adding holding homepage and serving static files

--- src/server/server.js
+++ src/server/server.js
@@ -4,6 +4,8 @@
 	var http = require("http");
 	var fs = require("fs");
 	var server;
+  var nodeStatic = require('node-static');
+  var staticServer = new nodeStatic.Server('./src/server/content/static');
 
 	exports.start = function(homepageToServe, notFoundPageToServe, portNumber, callback) {
 		if (!portNumber) throw new Error("Require port number");
@@ -12,7 +14,14 @@
 		server.on("request", function(request, response) {
 			if (request.url === '/' || request.url === '/index.html' ) {
 				serveFile(response, homepageToServe);
-			}
+		  }
+      else if (isStaticRequest) {
+        console.log('###############' + request.url)
+       var url = require('url');
+       var pathname = decodeURI(url.parse(request.url).pathname);
+        console.log(pathname);
+       staticServer.serve(request, response);
+      }
 			else {
 				response.statusCode = 404;
 				serveFile(response, notFoundPageToServe);
@@ -27,6 +36,11 @@
 			response.end(data);
 		});
 	}
+
+  function isStaticRequest(request) {
+    staticRequest = (request.url.match(/^\/(images|css)/));
+    return staticRequest;
+  }
 
 	exports.stop = function(callback) {
 		server.close(callback);
